edd105cb101d3a0504be5ad23c9d92a767039b7c
Esteban Lema <estebanlema@gmail.com>
2026-04-07 11:52:42 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import engine, Base, get_db
+from models import Item
+from schemas import ItemCreate, ItemResponse
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "My first API is working!"}
+
+@app.get("/items", response_model=list[ItemResponse])
+def get_items(db: Session = Depends(get_db)):
+    return db.query(Item).all()
+
+@app.get("/items/{item_id}", response_model=ItemResponse)
+def get_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+@app.post("/items", response_model=ItemResponse)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    new_item = Item(name=item.name, description=item.description)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+@app.put("/items/{item_id}", response_model=ItemResponse)
+def update_item(item_id: int, updated_item: ItemCreate, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    item.name = updated_item.name
+    item.description = updated_item.description
+    db.commit()
+    db.refresh(item)
+    return item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.delete(item)
+    db.commit()
+    return {"message": f"Item {item_id} deleted successfully"}
